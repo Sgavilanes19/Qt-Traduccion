abd22d4b608badb6388d44fff7558f9f19cacba1
Stefany Gavilanes <sgavilanesc1@est.ups.ecu.ec>
2023-01-25 12:28:03 -0500

    Portugues

---
+++ Calculadora_pr.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt" sourcelanguage="es_EC">
+<context>
+    <name>Principal</name>
+    <message>
+        <location filename="principal.ui" line="14"/>
+        <source>Calculadora</source>
+        <translation>Calculadora</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="23"/>
+        <source>Número 1:</source>
+        <translation>Número 1:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="41"/>
+        <source>Número 2:</source>
+        <translation>Número 2:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="59"/>
+        <source>Operación:</source>
+        <translation>Operação:</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="67"/>
+        <source>Sumar</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="72"/>
+        <source>Restar</source>
+        <translation>Subtrair</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="77"/>
+        <source>Multiplicar</source>
+        <translation>Multiplicar</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="82"/>
+        <source>Dividir</source>
+        <translation>Dividir</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="107"/>
+        <source>Calcular</source>
+        <translation>Calcular</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="126"/>
+        <source>Archivo</source>
+        <translation>Arquivo</translation>
+    </message>
+    <message>
+        <location filename="principal.ui" line="134"/>
+        <source>Salir</source>
+        <translation>Saia</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="40"/>
+        <source>Resultado</source>
+        <translation>Resultado</translation>
+    </message>
+    <message>
+        <location filename="principal.cpp" line="41"/>
+        <source>El resultado es: %1</source>
+        <translation>O resultado é:%1</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
     QTranslator traducion;
     // Solicitando al usuario que seleccione un idioma
     QStringList idiomas;
-    idiomas << "Francés" << "Inglés" << "Español";
+    idiomas << "Francés" << "Inglés" << "Español" << "Portugués";
     QString idiomaSeleccionado = QInputDialog::getItem(NULL,
                                "Idioma",
                                "Seleccione un idioma",
@@ -21,6 +21,8 @@ int main(int argc, char *argv[])
         traducion.load(":/Calculadora_fr_FR.qm");
     }else if (idiomaSeleccionado == "Inglés"){
         traducion.load(":/Calculadora_en.qm");
+    }else if (idiomaSeleccionado == "Portugués"){
+        traducion.load(":/Calculadora_pr.qm");
     }
     // Si es diferente de español, se instala la traducción en TODA la aplicación
     if (idiomaSeleccionado != "Español"){
